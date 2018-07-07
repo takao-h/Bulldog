749d893b8af32c16a45d57b1d8104c7707cd0a8d
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-07-07 23:11:49 +0900

    add new func

--- bulldog/src/App.js
+++ bulldog/src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import chipLogo from './images/logo.png'
+import home_icon from './images/home_icon.svg'
+import present_icon from './images/present_icon.svg'
+import mypage_icon from './images/mypage_icon.svg'
+const header = () => {
+  return(
+    <header className="App-header">
+      <div id="logo">
+      <h1><img src={chipLogo}/></h1>
+      </div>
+    </header>
+
+  )
+}
+
+const footer = () => {
+  return(
+    <footer id="footer">
+      <ul className="flex-contents footer-list">
+        <li className="flex-list">
+          {/* <a href="#" class="active"> */}
+            <img src={home_icon} alt="ホーム"/>>
+            <p>ホーム</p>
+          {/* </a> */}
+        </li>
+        <li className="flex-list">
+          <div className="footer-main">
+            {/* <a href="#"> */}
+              <div className="footer-main-inner">
+                <img src={present_icon} alt="贈る"/>
+                <p>贈る</p>
+              </div>
+            {/* </a> */}
+          </div>
+        </li>
+        <li className="flex-list">
+          {/* <a href="#"> */}
+            <img src={mypage_icon} alt="マイページ"/>>
+            <p>マイページ</p>
+          {/* </a> */}
+        </li>
+      </ul>
+    </footer>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {header()}
+        {footer()}
       </div>
     );
   }
 }
 
-export default App;
+
+export default App; 
---
+++ bulldog/src/CardContest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from 'reactstrap';
+
+const CardContenrt = () => {
+  <Card>
+    <div className='flex-contents list-inner'>
+        <img src={}/>
+    </div>
+    
+  </Card>
+}
+export default CardContenrt;
